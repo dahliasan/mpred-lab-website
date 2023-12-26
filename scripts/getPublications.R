6f2a063c6df3709762b1d9b7ee24718798703d9d
dahliasan <laninaesloco@gmail.com>
2023-12-26 14:00:09 +1100

    ✨ Add ORCID integration and retrieve works

--- scripts/getPublications.R
+++ scripts/getPublications.R
@@ -1,40 +1,90 @@
-# Retrieve scholarly metadata from google scholar
-# scholar method ----------------------------------------------------------
-library(scholar)
 library(dplyr)
-scholar_id <- '8J8xm0kAAAAJ'
-publications <- get_publications(scholar_id) %>% tibble()
-
-publications
-pubid <- publications$pubid[1]
-get_article_scholar_url(scholar_id, pubid)
-
-
-
-
+library(tidyr)
 library(rcrossref)
-
-id <- "0000-0002-7823-7185"
-metadata <- rcrossref::cr_works(filter = c(orcid=orcid_id), limit = 1000, sort = "published")
-metadata
-pubs <- metadata %>% purrr::pluck("data")
-pubs
-
 library(rorcid)
+
+# Set up ORCID token if not already done
 # usethis::edit_r_environ()
 # Sys.getenv("ORCID_TOKEN")
-id <- "0000-0002-7823-7185"
+
+# convert yaml to df tibble
+people <- yaml::read_yaml("db/members.yaml") |>
+   purrr::map(function(x) {
+      # Loop through each element of the list
+      x <- lapply(x, function(y) {
+         if (is.null(y)) {
+            # Replace NULL with NA of type character
+            y <- NA_character_
+         }
+         return(y)
+      })
+      
+      x <- as_tibble(x)
+      x$yearCompleted <- as.numeric(x$yearCompleted)
+      return(x)
+   }) |> 
+   bind_rows()
+  
+
+# Extract orcid ID from orcidUrl
+people <- people |> 
+   mutate(orcid_id = stringr::str_split(orcidUrl, "/")) |> 
+   rowwise() |> 
+   mutate(orcid_id = orcid_id |> last())
+
+# Get ORCID works
 orcid_auth(client_id = Sys.getenv("ORCID_CLIENT_ID"), client_secret = Sys.getenv("ORCID_CLIENT_SECRET"), reauth = TRUE)
-orworks <- works(as.orcid(id))
 
-fields <- c("title", 
-            "is.referenced.by.count",
-            "abstract",
-            "author",
-            "url")
+# Get works for each person
+people_orcid <- people |> 
+   filter(!is.na(orcid_id)) |>
+   select(name, orcid_id) |> 
+   group_by(name, orcid_id) |> 
+   mutate(works = works(as.orcid(orcid_id)) |> as_tibble() |> list()) 
 
-res <- cr_works(dois = orworks$url.value[1:5], .progress='text')
-crworks <- purrr::pluck(res, "data") %>% 
-  select(title, is.referenced.by.count, abstract, author, url)
+
+people_orcid <- people_orcid |> 
+   unnest_wider(works) |> 
+   select(name, 
+          orcid_id, 
+          type, 
+          `external-ids.external-id`) |> 
+   unnest_longer(c(`external-ids.external-id`, type)) |> 
+   unnest_wider(`external-ids.external-id`) |> 
+   unnest_longer(c(`external-id-type`, `external-id-value`)) |> 
+   mutate(doi = `external-id-value`) |>
+   filter(`external-id-type` == "doi") |> 
+   select(name, 
+          orcid_id, 
+          type, 
+          doi)
+
+people_orcid <- people_orcid |> 
+   distinct(doi)
+   
+   
+people_cr <- cr_works(dois = people_orcid$doi, .progress='text') 
+# save(people_cr, file = "db/crworks.RData")   
+people_cr <- people_cr |> purrr::pluck("data")
+
+people_final <- people_cr |> 
+   select(title, doi, type, created, is.referenced.by.count, abstract, author, url, container.title) |> 
+   janitor::clean_names() 
+
+people_final <- people_final |> 
+   mutate(authors = purrr::map(author, ~paste0(.x$given, " ", .x$family, collapse = ", "))) |> 
+   unnest(authors) 
+
+# convert JAT xml to html
+
+
+# create embeddings for abstract and research themes.
+
+# pass abstract to openai api to categorise each publication into research themes:
+
+# save as yaml db 
+
+
+
 
 
