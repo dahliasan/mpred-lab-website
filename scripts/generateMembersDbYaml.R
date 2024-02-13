3ac5cf48fb36ae1e96814b31d01005cf64b35dd6
dahliasan <laninaesloco@gmail.com>
2024-02-13 15:12:38 +1100

    Replace NA values with ~ in generated YAML file

--- scripts/generateMembersDbYaml.R
+++ scripts/generateMembersDbYaml.R
@@ -12,5 +12,9 @@ data <- read_csv("db/members.csv")
 # Remove list names
 yaml <- as.yaml(data, column.major=FALSE)
 
+# Replace na.character and na.real with ~
+yaml <- gsub(".na.character", "~", yaml)
+yaml <- gsub(".na.real", "~", yaml)
+
 # Save the YAML string to a file
 write(yaml, file = "db/members.yaml")
